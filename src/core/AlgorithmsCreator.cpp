17a35aa6a0ffa99d68788af189ac3ca0a269ee3d
m4xxx1m <mi4469228@yandex.ru>
2025-01-29 00:11:55 +0300

    Add AlgorithmsCreator class

---
+++ src/core/AlgorithmsCreator.cpp
@@ -0,0 +1,24 @@
+#include "AlgorithmsCreator.hpp"
+
+#include <stdexcept>
+
+#include "../../lib/NaiveSubstringSearcher.hpp"
+
+AlgorithmList AlgorithmsCreator::CreateAlgorithms(const std::vector<std::string>& algorithmsNames) {
+    AlgorithmList algorithmList;
+    for (const auto& name : algorithmsNames) {
+        std::optional<SubstringSearchAlgorithm> algorithm =
+            BaseSubstringSearcher::GetAlgorithmByName(name);
+
+        if (algorithm.has_value()) {
+            switch (algorithm.value()) {
+                case SubstringSearchAlgorithm::kNaive:
+                    algorithmList.push_back(std::make_unique<NaiveSubstringSearcher>());
+                    break;
+            }
+
+        } else {
+            throw std::invalid_argument(name + " is not in list of implemented algorithms");
+        }
+    }
+}
---
+++ src/core/AlgorithmsCreator.hpp
@@ -0,0 +1,15 @@
+#ifndef ALGORITHMS_CREATOR_HPP_
+#define ALGORITHMS_CREATOR_HPP_
+
+#include <memory>
+#include <vector>
+
+#include "../../lib/BaseSubstringSearcher.hpp"
+
+using AlgorithmList = std::vector<std::unique_ptr<BaseSubstringSearcher>>;
+
+class AlgorithmsCreator {
+    AlgorithmList CreateAlgorithms(const std::vector<std::string> &algorithmsNames);
+};
+
+#endif
