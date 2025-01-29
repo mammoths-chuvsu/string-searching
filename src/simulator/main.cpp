ca54dbfdf6da7a3d453c7e6183286520d9b78ca8
Matvey Smirnov <111080682+Vdaleke@users.noreply.github.com>
2025-01-29 21:49:07 +0300

    feat: add cli (#10)
    Provide main.cpp with basic classes calls

--- src/simulator/main.cpp
+++ src/simulator/main.cpp
@@ -1,2 +1,27 @@
+#include <iostream>
 
-int main(int argc, char** argv) { return 0; }
+#include "argument_parser.hpp"
+#include "simulator/algorithms_creator.hpp"
+#include "simulator/simulator.hpp"
+#include "simulator/utils.hpp"
+
+int main(int argc, char** argv) {
+    ArgumentParser parser(argc, argv);
+
+    std::vector<std::string> algorithm_names = parser.GetAlgorithmsNames();
+    std::string substring = parser.GetSubstring();
+    std::string text = parser.GetText();
+
+    if (algorithm_names.empty() || substring.empty() || text.empty()) {
+        std::cerr << "Missing required arguments. Use --help for more information.\n";
+        return 1;
+    }
+
+    AlgorithmList algorithms = AlgorithmsCreator::CreateAlgorithms(algorithm_names);
+
+    Simulator simulator(std::move(algorithms));
+
+    simulator.Run(text, substring, std::cout);
+
+    return 0;
+}
