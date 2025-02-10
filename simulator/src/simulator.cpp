1a2d81c905583dc19dd6c4939a99a25e8a24676d
FirePancake1177 <poiskov2016@gmail.com>
2025-02-11 00:15:01 +0300

    feat: add tests for lib

--- simulator/src/simulator.cpp
+++ simulator/src/simulator.cpp
@@ -1,12 +1,15 @@
 #include "simulator.hpp"
 
 #include <chrono>
+#include <iomanip>
 #include <iostream>
+#include <ctime>
 
 Simulator::Simulator(AlgorithmList algorithms) : algorithms_(std::move(algorithms)) {}
 
 void Simulator::Run(const std::string& text, const std::string& substring, std::ostream& output) {
     for (const auto& algorithm : algorithms_) {
+        auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
         const auto start = std::chrono::high_resolution_clock::now();
 
         const auto result = algorithm->Contains(text, substring);
@@ -14,9 +17,17 @@ void Simulator::Run(const std::string& text, const std::string& substring, std::
         const auto stop = std::chrono::high_resolution_clock::now();
         const auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
-        output << std::boolalpha;
-        output << algorithm->GetName() << '\n'
-               << result << '\n'
-               << diff.count() << "ms\n-----------\n";
+        output << "----------------------------\n";
+        output << "Timestamp: " << std::put_time(std::localtime(&now), "%Y-%m-%d %H:%M:%S") << "\n";
+        output << "Algorithm: " << algorithm->GetName() << "\n";
+        output << "Duration: " << diff.count() << " ms\n";
+
+        if (result) {
+            output << "Status: Substring found\n";
+        } else {
+            output << "Status: Substring not found\n";
+        }
+
+        output << "----------------------------\n\n";
     }
 }
