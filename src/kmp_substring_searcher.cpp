cbda2292148751bb60b71c507ffc7e55c742ec64
Maxim Ivanov <73167493+m4xxx1m@users.noreply.github.com>
2025-02-11 23:08:52 +0300

    feat: Add Knuth-Morris-Pratt algorithm (#19)

---
+++ src/kmp_substring_searcher.cpp
@@ -0,0 +1,33 @@
+#include <string-searching/kmp_substring_searcher.hpp>
+
+KmpSubstringSearcher::KmpSubstringSearcher() : BaseSubstringSearcher("KMP") {}
+
+std::vector<int> KmpSubstringSearcher::ComputePrefixFunction(const std::string& str) const {
+    std::vector<int> prefix(str.size(), 0);
+
+    for (int i = 1; i < str.length(); ++i) {
+        int k = prefix[i - 1];
+        while (k > 0 && str[i] != str[k]) {
+            k = prefix[k - 1];
+        }
+        if (str[i] == str[k]) {
+            ++k;
+        }
+        prefix[i] = k;
+    }
+
+    return prefix;
+}
+
+bool KmpSubstringSearcher::Contains(const std::string& text, const std::string& substring) const {
+    const std::string concatenated = substring + "\0" + text;
+    const auto prefix = ComputePrefixFunction(concatenated);
+
+    for (int index = 0; index < text.length(); ++index) {
+        if (prefix[substring.length() + index + 1] == substring.length()) {
+            return true;
+        }
+    }
+
+    return false;
+}
