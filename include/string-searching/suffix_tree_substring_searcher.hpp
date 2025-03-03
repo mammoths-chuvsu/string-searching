6dedd3e8cc9d74ec82f146dfa7c8eeeb194ea8f3
Brudlord <brudloord@yandex.ru>
2025-03-03 13:08:53 +0300

    Rollback changes

--- include/string-searching/suffix_tree_substring_searcher.hpp
+++ include/string-searching/suffix_tree_substring_searcher.hpp
@@ -1,66 +1,18 @@
 #ifndef STRING_SEARCHING_SUFFIX_TREE_SUBSTRING_SEARCHER_HPP
 #define STRING_SEARCHING_SUFFIX_TREE_SUBSTRING_SEARCHER_HPP
 
-#include <list>
 #include <string>
-#include <unordered_map>
 
 #include "base_substring_searcher.hpp"
 
-#define MAX_CHAR 256
-
 /**
- * \brief Implementation of the Suffix Tree algorithm for substring search.
+ * \brief Implementation of suffix tree string searching algorithm.
  */
 class SuffixTreeSubstringSearcher : public BaseSubstringSearcher {
 public:
     SuffixTreeSubstringSearcher();
+
     bool Contains(const std::string& text, const std::string& substring) const override;
-
-private:
-    class SuffixTrieNode {
-    private:
-        SuffixTrieNode* children[MAX_CHAR]{};
-        std::list<int>* indexes;
-
-    public:
-        SuffixTrieNode() {
-            // Create an empty linked list for indexes of
-            // suffixes starting from this node
-            indexes = new std::list<int>;
-
-            // Initialize all child pointers as NULL
-            for (auto& i : children) i = nullptr;
-        }
-
-        // A recursive function to insert a suffix of the txt
-        // in subtree rooted with this node
-        void insertSuffix(std::string suffix, int index);
-
-        // A function to search a pattern in subtree rooted
-        // with this node.The function returns pointer to a linked
-        // list containing all indexes where pattern is present.
-        // The returned indexes are indexes of last characters
-        // of matched text.
-        bool search(std::string pat);
-    };
-
-    class SuffixTrie {
-    private:
-        SuffixTrieNode root;
-
-    public:
-        // Constructor (Builds a trie of suffixes of the given text)
-        SuffixTrie(const std::string& txt) {
-            // Consider all suffixes of given string and insert
-            // them into the Suffix Trie using recursive function
-            // insertSuffix() in SuffixTrieNode class
-            for (int i = 0; i < txt.length(); i++) root.insertSuffix(txt.substr(i), i);
-        }
-
-        // Function to searches a pattern in this suffix trie.
-        bool search(std::string pat);
-    };
 };
 
 #endif  // STRING_SEARCHING_SUFFIX_TREE_SUBSTRING_SEARCHER_HPP
--- src/suffix_tree_substring_searcher.cpp
+++ src/suffix_tree_substring_searcher.cpp
@@ -1,50 +1,22 @@
 #include <string-searching/suffix_tree_substring_searcher.hpp>
-#include <utility>
 
 SuffixTreeSubstringSearcher::SuffixTreeSubstringSearcher() : BaseSubstringSearcher("suffix-tree") {}
 
 bool SuffixTreeSubstringSearcher::Contains(const std::string& text,
                                            const std::string& substring) const {
     if (substring.empty()) return true;
-    if (text.size() < substring.size()) return false;
+    if (text.length() < substring.length()) return false;
 
-    // Build the suffix tree for the full text and search for the substring.
-    SuffixTrie tree(text);
-    return tree.search(substring);
-}
-
-// A recursive function to insert a suffix of the txt in
-// subtree rooted with this node
-void SuffixTreeSubstringSearcher::SuffixTrieNode::insertSuffix(std::string suffix, int index) {
-    // Store index in linked list
-    indexes->push_back(index);
-
-    // If string has more characters
-    if (!suffix.empty()) {
-        // Find the first character
-        char cIndex = suffix.at(0);
-
-        // If there is no edge for this character, add a new edge
-        if (children[cIndex] == nullptr) children[cIndex] = new SuffixTrieNode();
-
-        // Recur for next suffix
-        children[cIndex]->insertSuffix(suffix.substr(1), index + 1);
+    const size_t max_index = text.length() - substring.length();
+    for (size_t i = 0; i <= max_index; ++i) {
+        bool match = true;
+        for (size_t j = 0; j < substring.length(); ++j) {
+            if (text[i + j] != substring[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) return true;
     }
-}
-
-// A recursive function to search a pattern in subtree rooted with
-// this node
-bool SuffixTreeSubstringSearcher::SuffixTrieNode::search(std::string pat) {
-    if (pat.empty()) {
-        return indexes;
-    }
-
-    if (children[pat.at(0)] != nullptr) {
-        return (children[pat.at(0)])->search(pat.substr(1));
-    } else {
-        return false;
-    }
-}
-bool SuffixTreeSubstringSearcher::SuffixTrie::search(std::string pat) {
-    return root.search(std::move(pat));
+    return false;
 }
