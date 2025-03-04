ecf6af4124fe2f1a3c0748116b45ff2d3629ab0d
m4xxx1m <mi4469228@yandex.ru>
2025-03-04 22:58:04 +0300

    feat: add iterations in benchmark test

--- simulator/src/simulator.cpp
+++ simulator/src/simulator.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <cmath>
 #include <ctime>
 #include <iomanip>
 #include <iostream>
@@ -6,20 +7,36 @@
 
 Simulator::Simulator(AlgorithmList algorithms) : algorithms_(std::move(algorithms)) {}
 
-void Simulator::Run(const std::string& text, const std::string& substring, std::ostream& output) {
+void Simulator::Run(const std::string& text, const std::string& substring, std::ostream& output,
+                    int iterations) {
     for (const auto& algorithm : algorithms_) {
         auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        const auto start = std::chrono::high_resolution_clock::now();
+        auto start = std::chrono::high_resolution_clock::now();
+        bool result;
+        double mean = 0.0;
+        double deviation = 0.0;
 
-        const auto result = algorithm->Contains(text, substring);
+        for (int iter = 0; iter < iterations; ++iter) {
+            result = algorithm->Contains(text, substring);
+            const auto stop = std::chrono::high_resolution_clock::now();
+            const auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+            start = stop;
+            mean += diff.count();
+            deviation += std::pow(diff.count(), 2);
+        }
 
-        const auto stop = std::chrono::high_resolution_clock::now();
-        const auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+        mean /= iterations;
+        deviation = deviation / iterations - std::pow(mean, 2);
 
         output << "----------------------------\n";
         output << "Timestamp: " << std::put_time(std::localtime(&now), "%Y-%m-%d %H:%M:%S") << "\n";
         output << "Algorithm: " << algorithm->GetName() << "\n";
-        output << "Duration: " << diff.count() << " ms\n";
+        if (iterations == 1) {
+            output << "Duration: " << mean << " ms\n";
+        } else {
+            output << "Duration mean: " << mean << " ms\n";
+            output << "Duration standard deviation: " << deviation << " ms\n";
+        }
 
         if (result) {
             output << "Status: Substring found\n";
